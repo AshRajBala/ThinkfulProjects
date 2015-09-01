975fed58edeec8576ca85a40e44092861815a7dc
AshRajBala <shunyata.aishwarya@gmail.com>
2015-09-01 16:16:45 -0500

    Initial commit

---
+++ chi_squared.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 16:01:01 2015
+
+@author: AshRajBala
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats
+import collections
+
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+plt.show()
+chi2, p = scipy.stats.chisquare([freq.values()])
+msg = "Test Statistic: {}\np-value: {}"
+print(msg.format( chi2, p))
